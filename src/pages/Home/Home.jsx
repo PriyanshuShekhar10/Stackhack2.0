e7df7a6effc8e09efbcbb4c962bf7bb8c91d9e04
Priyanshu <priyanshushekhar100@gmail.com>
2024-08-15 19:12:04 +0530

    file/folder setup

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import Footer from "../../components/Footer/Footer"
+import Navbar from "../../components/Navbar/Navbar"
+
+export const Home = () => {
+  return (
+
+      <>
+      <Navbar/>
+      <div>Home</div>
+      <Footer/>
+      </>
+  )
+}
+
+
+export default Home
