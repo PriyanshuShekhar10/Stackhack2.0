9f76209253f1d3379d9355a30aa7ab55b1216b5a
Priyanshu <priyanshushekhar100@gmail.com>
2024-08-22 21:44:20 +0530

    link

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
         setLoading(true);
 
         try {
-            const response = await axios.post('http://localhost:8000/auth/login', {
+            const response = await axios.post('https://stackhack2-0-backend.onrender.com/movie/auth/login', {
                 email,
                 password
             }, {
